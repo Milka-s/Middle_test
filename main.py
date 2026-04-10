b5f112025651133f7490aa262fcfd5a2618f7111
Milka-s <liza12052006@gmail.com>
2026-04-10 12:35:18 +0300

    Додано метод sort_by_area для сортування за площею

--- main.py
+++ main.py
@@ -10,6 +10,9 @@ class PopulationAnalyzer:
         if not os.path.exists(self.file_path):
             raise FileNotFoundError(f"Файл {self.file_path} не знайдено.")
 
+        def sort_by_area(self, reverse=False):
+            return sorted(self.data, key=lambda x: x['area'], reverse=reverse)
+
         with open(self.file_path, 'r', encoding='utf-8') as file:
             for line in file:
                 line = line.strip()
