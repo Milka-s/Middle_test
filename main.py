df153db797d782ce5ea7a546c0e9a4dae436dd65
Milka-s <liza12052006@gmail.com>
2026-04-10 12:30:55 +0300

    Додати клас PopulationAnalyzer та метод read_file ну і створила main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+import os
+
+
+class PopulationAnalyzer:
+    def __init__(self, file_path):
+        self.file_path = file_path
+        self.data = []
+
+    def read_file(self):
+        if not os.path.exists(self.file_path):
+            raise FileNotFoundError(f"Файл {self.file_path} не знайдено.")
+
+        with open(self.file_path, 'r', encoding='utf-8') as file:
+            for line in file:
+                line = line.strip()
+                if line:
+                    name, area, population = line.split(',')
+
+                    self.data.append({
+                        'name': name.strip(),
+                        'area': float(area.strip()),
+                        'population': int(population.strip())
+                    })
+        return self.data
